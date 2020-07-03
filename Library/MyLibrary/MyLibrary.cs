9c2cd1720fdd4f274bd377e631c8df152adf7c84
Patryk Lotzwi <plotzwi@objectivity.co.uk>
2020-07-04 00:28:02 +0200

    Added sample library

---
+++ Library/MyLibrary/MyLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class MyLibrary
+    {
+        public void DoSomethingAwesome()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
